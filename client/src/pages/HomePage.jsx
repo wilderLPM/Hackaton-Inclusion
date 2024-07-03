795061da7baab99acd5d7d9146ae578d848ec432
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-07-03 12:20:32 +0200

    created NavigationArrows component, to navigate between musics

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@
 import AudioPlayer from "react-h5-audio-player";
 import Testimony from "../components/testimony/Testimony";
 import Button from "../components/Button";
+import NavigationArrows from "../components/NavigationArrows";
 import "react-h5-audio-player/lib/styles.css";
 import intro from "../../public/music/intro.mp3";
 
@@ -11,6 +12,7 @@ export default function HomePage() {
       <h1>L'histoire de Jean michel</h1>
       <Testimony />
       <Button text="Histoire interactive" isLink />
+      <NavigationArrows />
       <div>
         <AudioPlayer
           autoPlay
