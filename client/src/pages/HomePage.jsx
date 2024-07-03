71a66b3ab21e4a4a213d7bd9c113c47da90e895f
Pat <hoang.patrick@outlook.com>
2024-07-03 11:36:07 +0200

    ajout music player

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,19 @@
-export default function HomePage(){
-    return (<h1>Inclusion</h1>);
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import intro from "../../public/music/intro.mp3";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Inclusion</h1>
+      <div>
+        <AudioPlayer
+          autoPlay
+          src={intro}
+          // onPlay={console.log("onPlay")}
+          // other props here
+        />
+      </div>
+    </div>
+  );
 }
