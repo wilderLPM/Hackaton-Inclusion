810aef48d0c9ade9d6e32b6e1235c1fb3ed099fe
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-07-03 11:29:45 +0200

    added testimony component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
-export default function HomePage(){
-    return (<h1>Inclusion</h1>);
+import Testimony from "../components/testimony/Testimony";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>L'histoire de Jean michel</h1>
+      <Testimony />
+    </>
+  );
 }
