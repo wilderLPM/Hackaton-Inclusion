db42fb51f029f0dd548a73a79756d3c0824996b2
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-07-03 14:28:38 +0200

    css player

--- client/src/components/music/Music.jsx
+++ client/src/components/music/Music.jsx
@@ -1,10 +1,18 @@
+import AudioPlayer from "react-h5-audio-player";
 import "./Music.css";
 import Note from "../../assets/music.png";
+import intro from "../../../public/music/intro.mp3";
 
 export default function Music() {
   return (
     <div id="musicDiv">
       <img src={Note} alt="note de musique sur fond orange" id="MusicImg" />
+      <AudioPlayer
+          autoPlay
+          src={intro}
+          // onPlay={console.log("onPlay")}
+          // other props here
+        />
     </div>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import AudioPlayer from "react-h5-audio-player";
 import Testimony from "../components/testimony/Testimony";
 import Music from "../components/music/Music";
 import "./HomePage.css";
 import Button from "../components/Button";
 import NavigationArrows from "../components/NavigationArrows";
 import "react-h5-audio-player/lib/styles.css";
-import intro from "../../public/music/intro.mp3";
 
 export default function HomePage() {
   const name = "Jean Michel";
@@ -19,14 +17,6 @@ export default function HomePage() {
       </main>
       <Button text="Histoire interactive" isLink />
       <NavigationArrows />
-      <div>
-        <AudioPlayer
-          autoPlay
-          src={intro}
-          // onPlay={console.log("onPlay")}
-          // other props here
-        />
-      </div>
     </>
   );
 }
