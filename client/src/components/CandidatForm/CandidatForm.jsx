41e4afc6436e2158281e3424635d1d780dc48978
Pat <hoang.patrick@outlook.com>
2024-07-04 10:16:25 +0200

    ok

--- client/src/components/CandidatForm/CandidatForm.jsx
+++ client/src/components/CandidatForm/CandidatForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./CandidatForm.css";
+import styles from "./CandidatForm.module.css";
 
 export default function CandidatForm() {
   const [nom, setNom] = useState("");
--- client/src/components/NavigationArrows.jsx
+++ client/src/components/NavigationArrows.jsx
@@ -1,40 +1,40 @@
-// import styles from "./NavigationArrows.module.css";
+import styles from "./NavigationArrows.module.css";
 
-// // eslint-disable-next-line react/prop-types
-// export default function NavigationArrows({
-//   musicPlayed,
-//   setMusicPlayed,
-//   playlistLength,
-// }) {
-//   const handleClick = (e) => {
-//     if (e.target.value === "previous" && musicPlayed > 0) {
-//       setMusicPlayed(musicPlayed - 1);
-//     } else if (
-//       e.target.value === "previous" &&
-//       musicPlayed < playlistLength - 1
-//     ) {
-//       setMusicPlayed(musicPlayed + 1);
-//     }
-//   };
+// eslint-disable-next-line react/prop-types
+export default function NavigationArrows({
+  musicPlayed,
+  setMusicPlayed,
+  playlistLength,
+}) {
+  const handleClick = (e) => {
+    if (e.target.value === "previous" && musicPlayed > 0) {
+      setMusicPlayed(musicPlayed - 1);
+    } else if (
+      e.target.value === "previous" &&
+      musicPlayed < playlistLength - 1
+    ) {
+      setMusicPlayed(musicPlayed + 1);
+    }
+  };
 
-//   return (
-//     <div className={styles.navigationArrows}>
-//       <button
-//         type="button"
-//         onClick={handleClick}
-//         value="previous"
-//         className={styles.arrowPrevious}
-//       >
-//         &lt;
-//       </button>
-//       <button
-//         type="button"
-//         onClick={handleClick}
-//         value="next"
-//         className={styles.arrowNext}
-//       >
-//         &gt;
-//       </button>
-//     </div>
-//   );
-// }
+  return (
+    <div className={styles.navigationArrows}>
+      <button
+        type="button"
+        onClick={handleClick}
+        value="previous"
+        className={styles.arrowPrevious}
+      >
+        &lt;
+      </button>
+      <button
+        type="button"
+        onClick={handleClick}
+        value="next"
+        className={styles.arrowNext}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+}
