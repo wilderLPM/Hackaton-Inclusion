3acd2ef5d2822d7e2db9b917b89a675477d10294
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-07-04 09:35:01 +0200

    ouais

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,24 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import Testimony from "../components/testimony/Testimony";
-import Music from "../components/music/Music";
-import "./HomePage.css";
+// import Form1
+// import Form2
 import Button from "../components/Button";
-import NavigationArrows from "../components/NavigationArrows";
-import "react-h5-audio-player/lib/styles.css";
 
 export default function HomePage() {
-  const name = "Jean Michel";
   return (
     <>
-      <h1>L'histoire de {name}</h1>
-      <main id="mainPage">
-        <Testimony />
-        <Music />
-      </main>
-      <div id="buttonDiv" >
-      <Button text={`Découvrir le CV de ${name}`} isLink />
+      <h1>Échos d'ailleurs</h1>
+      <div id="buttonDiv">
+        <Button text={`S'inscrire en tant que candidat`} isLink />
+        <Button text={`S'inscrire en tant que recruteur`} isLink />
       </div>
-      <NavigationArrows />
       {/* <Button text="Histoire interactive" isLink /> */}
     </>
   );
---
+++ client/src/pages/SoulPhage.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Testimony from "../components/testimony/Testimony";
+import Music from "../components/music/Music";
+import "./HomePage.css";
+import Button from "../components/Button";
+import NavigationArrows from "../components/NavigationArrows";
+import "react-h5-audio-player/lib/styles.css";
+
+export default function SoulPhage() {
+  const name = "Jean Michel";
+  return (
+    <>
+      <h1>L'histoire de {name}</h1>
+      <main id="mainPage">
+        <Testimony />
+        <Music />
+      </main>
+      <div id="buttonDiv">
+        <Button text={`Découvrir le CV de ${name}`} isLink />
+      </div>
+      <NavigationArrows />
+      {/* <Button text="Histoire interactive" isLink /> */}
+    </>
+  );
+}
